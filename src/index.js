47902e50d4dfdecd86c02ac93eca3b467ed4a32d
David Giraldo <davidgiraldobueno@gmail.com>
2019-12-28 18:46:07 -0500

    finish

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import {choice, remove} from './helpers'
 
 let fruit = choice(foods);
 
-console.log("The fruit", fruit)
+console.log(`I'd like one ${fruit}, please`);
+
+console.log(`Here you go: ${fruit}`);
+
+let remaining = remove(foods,fruit);
+
+console.log(`I'm sorry we're all out. We have   ${remaining.length} other fruits left.`);
+
